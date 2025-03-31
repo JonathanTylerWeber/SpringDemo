b3c893ad447e90521d2879a413b2313f45a51742
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-03-31 19:42:07 +0800

    add message service bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext myAppContext = SpringApplication.run(DemoApplication.class, args);
+
+		for (String beanName : myAppContext.getBeanDefinitionNames()){
+			System.out.println(beanName);
+		}
+
+		MessageService messageService = myAppContext.getBean(MessageService.class);
+
+		System.out.println(messageService.getMessage());
+
 	}
 
 }
